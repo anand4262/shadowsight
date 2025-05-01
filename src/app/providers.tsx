4c4e95f333820d5e848162ed67ddd564f6f9fdc9
Mahesh Anand <anands@Maheshs-MacBook-Pro.local>
2025-05-01 23:33:26 +1000

    Got rid of redux-persist

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,17 +3,14 @@
 import { SidebarProvider } from "@/components/Layouts/sidebar/sidebar-context";
 import { ThemeProvider } from "next-themes";
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import store,  {persistor} from '@/store/Store'; 
+import store from '@/store/Store'; 
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <SidebarProvider>
         <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
               {children}
-          </PersistGate>
         </Provider>
       </SidebarProvider>
     </ThemeProvider>
--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,25 +1,15 @@
 // store/index.ts
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import CSVReducer from './slice/CSVSlice';
 import uploadReducer from '@/store/slice/uploadSlice'
 
-const persistConfig = {
-    key: 'root',  
-    storage,     
-  };
-
-  const persistedReducer = persistReducer(persistConfig, CSVReducer);
-
 const store = configureStore({
   reducer: {
-    csv: persistedReducer, 
+    csv: CSVReducer, 
     uploads: uploadReducer
   },
 });
 
-export const persistor = persistStore(store);
 export type AppStore = typeof store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch =  AppStore['dispatch']

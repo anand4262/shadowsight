ff7d3817992a6daf71275aac203efcd40581e443
Mahesh Anand <anands@Mac.lan>
2025-05-16 12:08:07 +1000

    GH Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  basePath: '/shadowsight', // important!
+  assetPrefix: '/shadowsight/', // important!
   images: {
     domains: ["localhost"],
     remotePatterns: [
